910c01f98c9bcc7ae4ccb16b26e954490eb8fa98
Kateryna Troyan <ket.tro@gmail.com>
2025-09-08 15:02:14 +0300

    add base routes /, /catalog, /catalog/:id with Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
+
+import Layout from './components/Layout/Layout';
+
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const Catalog = lazy(() => import('./pages/Catalog/Catalog'));
-const CarDetails = lazy(() =>
-  import('./pages/CarDetails/CarDetails')
-);
+const CarDetails = lazy(() => import('./pages/CarDetails/CarDetails'));
 const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
 //const Navigation = lazy(() => import('./components/Navigation/Navigation'));
@@ -16,10 +17,12 @@ export default function App() {
       <Suspense fallback={<div>Loading page...</div>}>
         <Navigation />
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/cars" element={<Catalog />} />
-          <Route path="/movies/:id" element={<CarDetails />}/>
-          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/cars" element={<Catalog />} />
+            <Route path="/cars/:id" element={<CarDetails />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
         </Routes>
       </Suspense>
     </>
