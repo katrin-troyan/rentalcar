fd39c9ecd1a87902a0afa1d92f50a04743f6e6f1
Kateryna Troyan <ket.tro@gmail.com>
2025-09-08 14:22:38 +0300

    project structure

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
-KinoClick - the world of cinema in your hands!
 
-Welcome to KinoClick! Here you can easily find your favourite films and TV
-shows. Search by title, genre, rating or year of release - discover the best
-cinema in a few clicks.
-
-🎬 Dive into the world of cinema now!
-
-Convenient film search Simply enter the title of a film, actor or director and
-our smart search will instantly show you the best results.
-
-Filters for your convenience Choose movies by genre, year of release, rating or
-popularity. Find exactly what you want to watch!
-
-Reviews and ratings Read reviews from other viewers, view ratings and make your
-decision easier.
--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
-const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
-const MovieDetailsPage = lazy(() =>
-  import('./pages/MovieDetailsPage/MovieDetailsPage')
+const Catalog = lazy(() => import('./pages/Catalog/Catalog'));
+const CarDetails = lazy(() =>
+  import('./pages/CarDetails/CarDetails')
 );
 const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
-const MovieCast = lazy(() => import('./components/MovieCast/MovieCast'));
-const MovieReviews = lazy(() =>
-  import('./components/MovieReviews/MovieReviews')
-);
-const Navigation = lazy(() => import('./components/Navigation/Navigation'));
+
+//const Navigation = lazy(() => import('./components/Navigation/Navigation'));
 
 export default function App() {
   return (
@@ -20,11 +17,8 @@ export default function App() {
         <Navigation />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-            <Route path="cast" element={<MovieCast />} />
-            <Route path="reviews" element={<MovieReviews />} />
-          </Route>
+          <Route path="/cars" element={<Catalog />} />
+          <Route path="/movies/:id" element={<CarDetails />}/>
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
