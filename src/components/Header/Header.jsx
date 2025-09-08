81949720e1fd442d636a4783dd48dcbb52c8de56
Kateryna Troyan <ket.tro@gmail.com>
2025-09-08 15:11:39 +0300

    add Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import {  Link, NavLink } from 'react-router-dom';
+import clsx from 'clsx';
+
+export default function Header() {
+  return (
+    <header>
+      <div className="logo">
+          <Link to="/">MyLogo</Link>
+        </div>
+        <nav className="nav">
+          <NavLink
+          to="/"
+          className={({ isActive }) =>
+            clsx(css.link, { [css.active]: isActive })
+          }
+        >
+          Home
+        </NavLink>
+          <NavLink
+             to="/catalog"
+            className={({ isActive }) =>
+              clsx(css.link, { [css.active]: isActive })
+            }
+          >
+           Catalog
+          </NavLink>
+        </nav>
+    </header>
+  );
+}
