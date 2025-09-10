6e6a6704dcfe8e3a64f3fbac22af9ef9d8d94d06
Kateryna Troyan <ket.tro@gmail.com>
2025-09-10 22:19:13 +0300

    add favicon, README

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
+# RentalCar
 
+**Short Description:**  
+RentalCar is a web application for car rentals that allows users to browse available vehicles, apply filters, view detailed car information, and add favorite cars to a personal list.
+
+## Key Features
+- Browse a list of cars with pagination.
+- Filter cars by brand, price, and mileage.
+- View detailed information about each car.
+- Add or remove cars from the favorites list.
+- Favorites are stored locally using `localStorage`.
+
+## Installation
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/katrin-troyan/rentalcar
+
+2. Navigate to the project folder:
+    cd rentalcar
+
+3. Install dependencies:
+    npm install
+4. Start the development server:
+    npm run dev
+
+## Usage
+
+- Open your browser and go to http://localhost:5173.
+- Use the filters to find the desired car.
+- Click on a car to view its detailed information.
+- Add cars to your favorites list for quick access later.
+
+## Author
+
+- Name: Kateryna
+- GitHub: https://github.com/katrin-troyan
--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,25 @@ import { lazy, Suspense } from 'react';
 import Layout from './components/Layout/Layout';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Loader from './components/Loader/Loader.jsx';
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
 const Catalog = lazy(() => import('./pages/Catalog/Catalog.jsx'));
 const CarDetails = lazy(() => import('./pages/CarDetails/CarDetails.jsx'));
-const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
 export default function App() {
   return (
     <>
-      <Suspense fallback={<div>Loading page...</div>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/catalog" element={<Catalog />} />
             <Route path="/catalog/:id" element={<CarDetails />} />
-            <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
       </Suspense>
-      <ToastContainer position="top-right" autoClose={1500} />
+      <ToastContainer position="top-right" autoClose={2000} />
       </>
   );
 }
